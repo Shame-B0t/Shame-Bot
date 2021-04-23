1e25a4c29f99447a7fdd0632f14ef3c8b69b0522
patrick hrabos <phrabos@gmail.com>
2021-04-23 12:09:17 -0400

    begin setup wizard functionality

--- utils/setupWizard.js
+++ utils/setupWizard.js
@@ -1,1 +1,27 @@
 // the bot configuration functionality
+
+function ifStart(message){
+  if(message.content.startsWith('!start')){
+
+    const [mode, timeoutLength] = message.content.split(' ').slice(1);
+    // const sender = message.author.username;
+    // const isActive = true;
+    // const shameLevel = 0;  
+      
+    switch(mode){
+      case 'easy':
+        break;
+      case 'medium':
+        break;
+      case 'sadistic':
+        break;
+      default: message.reply('not valid option, please type easy medium or sadistic'); 
+        return;
+    }
+    message.reply(` your level of ${mode} has been set.`);
+  }
+}
+
+module.exports = {
+  ifStart,
+};
