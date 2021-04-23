860ae0392665f4794def3c68944fcb434cd19ee6
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-23 12:06:34 -0700

    Merge updates

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Good to go, boss!');
+  console.log('Good to go, boss!');
 });
 
 client.login(process.env.TOKEN);
 
 client.on('message', message => {
-	if (message.author.bot) return;
-	if (message.content.includes('ping')) message.reply('pong!');
+  if(message.author.bot) return;
+  if(message.content.includes('ping')) message.reply('pong!');
 });
