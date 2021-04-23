fe91676f8841e520ed31a9eca45ebd9c1fabc5c1
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-23 16:31:17 -0700

    More merge conflicts

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,12 +1,5 @@
 describe('placeholder test', () => {
-<<<<<<< HEAD
   it('is true', () => {
     expect(true).toEqual(true);
   });
 });
-=======
-	it('is true', () => {
-		expect(true).toEqual(true);
-	});
-});
->>>>>>> 0833658997a12f3906833502f202fe6f74c50bba
