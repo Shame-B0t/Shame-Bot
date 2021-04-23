1fbc7a6289f42cd0cf82b02e5b520a8f78764bf8
Patrick Hrabos <phrabos@gmail.com>
2021-04-23 19:44:30 -0400

    Start function ph (#9)
    * npm i changed package-lock.json
    
    * begin setup wizard functionality
    
    * github workflows yaml
    
    * github workflows yaml
    
    * regex for time format testing

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client(); // make an instance of the Client class as our 'client'
+const { msgCallback } = require('./commands/start');
+
 
 client.once('ready', () => {
-  console.log('Good to go, boss!');
+  console.log('Good to go, boss!'); 
+  // check for and make channel if needed
+  // check for and make webhook if needed
 });
 
 client.login(process.env.TOKEN);
 
-client.on('message', message => {
-  if(message.author.bot) return;
-  if(message.content.includes('ping')) message.reply('pong!');
-});
+client.on('message', msgCallback);
