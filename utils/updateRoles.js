c2dbc0e8800f3604633fcac1cb32169486fe3aee
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-26 11:18:26 -0700

    Add isolated functions for creating, managing, and deleting roles

--- utils/updateRoles.js
+++ utils/updateRoles.js
@@ -1,1 +1,22 @@
 // strip off and reattach the user's roles
+
+const getUserRoles = ({ member }) => member.roles.cache.filter(role => role.name !== '@everyone');
+
+const stripUserRoles = async ({ member }, userRoles) => {
+  await Promise.all(userRoles.map(role => member.roles.remove(role)));
+};
+
+const assignNewRole = ({ member }, role) => {
+  return member.roles.add(role);
+};
+
+const restoreUserRoles = async ({ member }, userRoles) => {
+  await Promise.all(userRoles.map(role => member.roles.add(role)));
+};
+
+module.exports = {
+  getUserRoles,
+  stripUserRoles,
+  assignNewRole,
+  restoreUserRoles
+};
