246168aec9445a32981a242fe135520c854b723d
Casey Warren <caseywar1017@gmail.com>
2021-04-26 12:41:02 -0400

    commiting to pull

--- commands/help.js
+++ commands/help.js
@@ -1,1 +1,9 @@
 // response to "help": summarize commands and what to expect from the bot
+
+function helpMessage (message) {
+
+	if (message.author.bot) return;
+	if (message.content.includes('--help')) message.channel.send('Robinson!');
+};
+
+module.exports = { helpMessage };
