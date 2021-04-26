70581c69a68f12e051920249f4d17ec83b1ae923
Casey Warren <caseywar1017@gmail.com>
2021-04-26 16:13:26 -0400

    fixing es lint errors

--- commands/help.js
+++ commands/help.js
@@ -1,44 +1,48 @@
 // response to "help": summarize commands and what to expect from the bot
 const Discord = require('discord.js');
 
-function helpMessage (message) {
-	const helpMessageEmbed = new Discord.MessageEmbed()
-	.setColor('#38000c')
+function helpMessage(message) {
 
-	.setTitle('Description')
-	.setAuthor('Shame-b0t', 'https://avatars.githubusercontent.com/u/83033726?s=200&v=4', 'https://github.com/Shame-B0t/Shame-Bot')
-	.setDescription('Shame-bot is your personal productivity manager with an attitude. Use Shame-b0t to focus for a desired amount of time, with varying levels of strictness. ')
-	
-	.addField('note:', 'All commands must start with `--shame` and be followed by a “level of shame” and a time in the format of 00:00 (hours:minutes)')
-	
-	.addField( '\u200B', '\u200B' )
+  // eslint-disable-next-line keyword-spacing
+  if (message.author.bot || !message.content === '--help') return;
 
-	.addFields(
-		{ name: 'Shame', value: 'The shame level will **not** bar you from any channels. If Shame-b0t catches you talking anywhere in discord, you will be publicly shamed by the bot' },
-		{ name: 'Isolation', value: 'The isolation level will take you to a new, private channel and bar you from accessing any other channel during the desired focus time.' },
-		{ name: 'Lockdown', value: 'The lockdown level will take you to a new, private channel and bar access from all other channels. In order to **exit early** on lockdown mode, you must first complete a typing challenge. To start the challenge type `--challenge` then begin typing the required amount of words.' }
-	)
+  const helpMessageEmbed = new Discord.MessageEmbed()
+    .setColor('#38000c')
 
-	.addField( '\u200B', '\u200B' )
-	
-	.addFields(
-		{ name: 'Comand Formats', value: 'Type in commands under this format, including your desired focus time'},
-		{ name: 'Shame', value: '``--focus shame 00:00``', inline: false },
-		{ name: 'Isolation', value: '`--focus isolation 00:00`', inline: false },
-		{ name: 'Lockdown', value: '`--focus lockdown 00:00`', inline: false },
-		{ name: 'You may EXIT at anytime by entering the command:', value: '`--exit`'}
-	)
+    .setTitle('Description')
+    .setAuthor('Shame-b0t', 'https://avatars.githubusercontent.com/u/83033726?s=200&v=4', 'https://github.com/Shame-B0t/Shame-Bot')
+    .setDescription('Shame-bot is your personal productivity manager with an attitude. Use Shame-b0t to focus for a desired amount of time, with varying levels of strictness. ')
 
-	.addField( '\u200B', '\u200B' )
+    .addField('note:', 'All commands must start with `--shame` and be followed by a “level of shame” and a time in the format of 00:00 (hours:minutes)')
 
-	.addFields(
-		{ name: 'Away Messages', value: 'If another user tries to mention you in a comment, they will be sent an automated message notifying them that you are in focus mode'}
-	)
+    .addField('\u200B', '\u200B')
 
-	.setFooter('have a productive day!', 'https://avatars.githubusercontent.com/u/83033726?s=200&v=4');
+    .addFields(
+      { name: 'Shame', value: 'The shame level will **not** bar you from any channels. If Shame-b0t catches you talking anywhere in discord, you will be publicly shamed by the bot' },
+      { name: 'Isolation', value: 'The isolation level will take you to a new, private channel and bar you from accessing any other channel during the desired focus time.' },
+      { name: 'Lockdown', value: 'The lockdown level will take you to a new, private channel and bar access from all other channels. In order to **exit early** on lockdown mode, you must first complete a typing challenge. To start the challenge type `--challenge` then begin typing the required amount of words.' }
+    )
 
-	if (message.author.bot) return;
-	if (message.content.includes('--help')) message.channel.send(helpMessageEmbed);
-};
+    .addField('\u200B', '\u200B')
+
+    .addFields(
+      { name: 'Comand Formats', value: 'Type in commands under this format, including your desired focus time' },
+      { name: 'Shame', value: '``--focus shame 00:00``', inline: false },
+      { name: 'Isolation', value: '`--focus isolation 00:00`', inline: false },
+      { name: 'Lockdown', value: '`--focus lockdown 00:00`', inline: false },
+      { name: 'You may EXIT at anytime by entering the command:', value: '`--exit`' }
+    )
+
+    .addField('\u200B', '\u200B')
+
+    .addFields(
+      { name: 'Away Messages', value: 'If another user tries to mention you in a comment, they will be sent an automated message notifying them that you are in focus mode' }
+    )
+
+    .setFooter('have a productive day!', 'https://avatars.githubusercontent.com/u/83033726?s=200&v=4');
+
+  // eslint-disable-next-line keyword-spacing
+  if (message.content.includes('--help')) message.channel.send(helpMessageEmbed);
+}
 
 module.exports = { helpMessage };
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client(); // make an instance of the Client class as our 'client'
 const { ifStart } = require('./commands/start');
-const { helpMessage } = require('./commands/help')
+const { helpMessage } = require('./commands/help');
 const { ifExit } = require('./commands/stop');
 
 
@@ -18,7 +18,7 @@ client.login(process.env.TOKEN);
 client.on('message', (message) => {
   ifStart(message, client); //!focus
   ifExit(message); //!exit
-  helpMessage(message)
+  helpMessage(message);
 });
 
 
