d52501b95930d893f2565129b784860ec1131e39
patrick hrabos <phrabos@gmail.com>
2021-04-26 15:43:02 -0400

    challenge to escape lockdown mode implemented

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
+const { ifChallenge } = require('./commands/challenge');
 const client = new Discord.Client(); // make an instance of the Client class as our 'client'
 const { ifStart } = require('./commands/start');
 const { ifExit } = require('./commands/stop');
@@ -17,6 +18,8 @@ client.login(process.env.TOKEN);
 client.on('message', (message) => {
   ifStart(message, client); //!focus
   ifExit(message); //!exit
+  
+  ifChallenge(message); 
 });
 
 
