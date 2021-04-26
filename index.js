f74aa9c21e8e3b06e5c5e365c65870c99e375be7
Casey Warren <74938966+caseywar@users.noreply.github.com>
2021-04-26 16:27:19 -0400

    Casey branch (#17)
    * commiting to pull
    
    * fixing es lint errors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client(); // make an instance of the Client class as our 'client'
 const { ifStart } = require('./commands/start');
+const { helpMessage } = require('./commands/help');
 const { ifExit } = require('./commands/stop');
 
 
@@ -17,6 +18,7 @@ client.login(process.env.TOKEN);
 client.on('message', (message) => {
   ifStart(message, client); //!focus
   ifExit(message); //!exit
+  helpMessage(message);
 });
 
 
