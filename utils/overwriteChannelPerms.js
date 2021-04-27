bed6f5af36d978a5611b8ae48989589dcc0308db
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-26 21:16:53 -0700

    Refine permission overwrite function

--- utils/overwriteChannelPerms.js
+++ utils/overwriteChannelPerms.js
@@ -1,24 +1,25 @@
-const overwriteChannelPerms = ({ guild, author }) => {
+const overwriteChannelPerms = async ({ guild, author }) => {
   const { channels } = guild;
   const originalChannels = channels.cache;
-  
+ 
   originalChannels.forEach(channel => {
-	  channel.createOverwrite(author.id, {
-		  'VIEW_CHANNEL': false
-	  })
-	  .catch(() => {
-		  console.log(`Bot lacks access to perform an overwrite in ${channel.name}`);
-	  });
-  });
-
+    channel.createOverwrite(author.id, {
+      'VIEW_CHANNEL': false
+    })
+      .catch(() => {
+        console.log(`No overwrite performed in ${channel.name}`);
+      });
+  }); 
+  
+  // removing overwrites after a timeout for sake of testing/illustration
   setTimeout(() => {
     originalChannels.forEach(channel => {
-      channel.createOverwrite(author.id, {
-        'VIEW_CHANNEL': true
-      })
-        .catch(() => {
-          console.log(`Bot lacks access to perform an overwrite in ${channel.name}`);
-        });
+      try {
+        channel.permissionOverwrites.get(author.id).delete();
+      }
+      catch(error) {
+        console.log(`No delete of overwrites performed in ${channel.name}`);
+      }
     });
   }, 5000);
 };
