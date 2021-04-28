e40fdcf3d4358b96ea7d93a272b00d1a42ce4c9c
patrick hrabos <phrabos@gmail.com>
2021-04-28 15:49:40 -0400

    make new file for countdown

---
+++ commands/countdown.js
@@ -0,0 +1,28 @@
+// function to check remaining time on focus mode
+const { usersArray } = require('./start');
+const { msToString, remainingTime } = require('../utils/parseTime');
+
+function countdown(message) {
+  if(message.author.bot) return; 
+  if(message.content.startsWith('--time')) {
+  // check if user is in usersArray
+    const focusedUser = usersArray.find(activeUser =>
+      activeUser.userId === message.author.id);
+
+    if(!focusedUser) return;
+
+    // pull endtime and compare to now to get timeLeft
+    const endTime = focusedUser.endTime;
+    const timeLeft = remainingTime(endTime);
+    const timeLeftString = msToString(timeLeft);
+    // if time left < 1 min, show seconds
+
+    //   reply to message with time left
+    message.reply(`You have ${timeLeftString} left.`);
+  }
+}
+
+module.exports = { 
+  countdown,
+  
+};
