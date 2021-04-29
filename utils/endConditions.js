eee87a4f67ce503968acfb899ffcde2face4420d
patrick hrabos <phrabos@gmail.com>
2021-04-29 00:42:50 -0400

    revise endCondition logic so timer only delets when timer exists

--- utils/endConditions.js
+++ utils/endConditions.js
@@ -37,7 +37,11 @@ const cleanUp = async (usersArray) => {
       if(!user.isActive){
         // if(isBotRoleHigher({ member: user.member }))
         restoreNickname(user, user.member);
-        user.botTimerMessage.delete();
+
+        if(user.userSetTimer){
+          user.botTimerMessage.delete();
+
+        }
 	
         if(user.mode !== 'shame') {
           console.log('exited early');
@@ -51,7 +55,10 @@ const cleanUp = async (usersArray) => {
 			
         // if(isBotRoleHigher({ member: user.member })) 
         restoreNickname(user, user.member);
-        user.botTimerMessage.delete();
+        if(user.userSetTimer){
+          user.botTimerMessage.delete();
+
+        }
 	
         if(user.mode !== 'shame') {
           console.log('timer up');
