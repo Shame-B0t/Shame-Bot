e146c13584d2214c5428d00315ee2cea3933d99c
Devon Wolfkiel <devonwolf@tutanota.com>
2021-04-28 21:44:29 -0700

    Move destructuring into body of functions, remove some comments and old code

--- utils/newChannel.js
+++ utils/newChannel.js
@@ -1,19 +1,15 @@
 const { botReplies } = require('../data/shameReplies');
 
 const deleteChannel = channel => {
-  console.log('CHANNEL NAME', channel.name);
   channel.delete()
     .then(result => console.log(`Channel ${result.id} - ${result.name} DELETED`))
-    .catch(error => console.error('ERROR MESSAGE:', error));
+    .catch(console.error);
 };
 
 /*
 makes a new channel with the format 'username-focus'
-takes in the client and a message object - makeNewPrivateChannel(client, message)
-sets permission overwrites on the new channel so that only the triggering author and the bot can view it (and, by extension, send messages in it)
+sets permission overwrites on the new channel so that only the triggering author and the bot can view it
 sends an @ mention to the triggering user from the new channel after it is instantiated
-
-for test/illustration purposes, deleteChannel is being used here after a ten second timeout
 */
 
 const makeNewPrivateChannel = (client, message, userObj) => {
@@ -23,6 +19,7 @@ const makeNewPrivateChannel = (client, message, userObj) => {
   // maybe add in a react here on the original message that indicates it's been heard? in case the user misses the mention from the new channel? A lil ear emoji? Some studious person? A speech bubble? A telephone?
 
   // TODO add role permission for the newly created role, possibly
+  
   channels.create(`${ author.username }-focus`, {
     type: 'text',
     permissionOverwrites: [{
